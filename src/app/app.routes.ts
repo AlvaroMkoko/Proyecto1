daa86e5d6b8f73aa44f286c462c8641f4331d22e
Alvaro <velazquez.matus.alvaro@gmail.com>
2025-01-11 09:12:00 -0600

    Redireccion aplicada en app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { CarreraFormComponent } from './carrera-form/carrera-form.component';
 import { CarreraComponent } from './carrera/carrera.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'listadoCarreras', pathMatch: 'full' },
   {path:'listadoCarreras',component:CarreraComponent},
   {path:'carreraForm',component:CarreraFormComponent},
   {path:'carreraForm/:id',component:CarreraFormComponent}
