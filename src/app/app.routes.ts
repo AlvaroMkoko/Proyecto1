e27eeb75590711328eda73ec5dd39b95ef8efb63
Alvaro <velazquez.matus.alvaro@gmail.com>
2025-01-11 01:04:21 -0600

    Cambio ruta y css eliminado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { CarreraFormComponent } from './carrera-form/carrera-form.component';
 import { CarreraComponent } from './carrera/carrera.component';
 
 export const routes: Routes = [
+  {path:'listadoCarreras',component:CarreraComponent},
   {path:'carreraForm',component:CarreraFormComponent},
-  {path:'carreraForm/:id',component:CarreraFormComponent},
-  {path:'listadoCarreras',component:CarreraComponent}
+  {path:'carreraForm/:id',component:CarreraFormComponent}
 ];
